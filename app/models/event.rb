27927cae22823f9218171c772a539a2edfab37b4
calpas7000 <calpas7000@gmail.com>
2020-11-15 13:37:28 +0000

    event enum

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,17 @@ class Event < ApplicationRecord
   belongs_to :user
   
   validates :title, length: { maximum: 100 }
-  validates :place, length: { maximum: 255 }
   validates :event_date, presence: true
   validates :content, presence: true, length: { maximum: 4294967296 }
+  
+  enum other: { other_f: false, other_t: true }
+  enum pc: { pc_f: false, pc_t: true }
+  enum ps4: { ps4_f: false, ps4_t: true }
+  enum ps5: { ps5_f: false, ps5_t: true }
+  enum xbox_one: { xbox_one_f: false, xbox_one_t: true }
+  enum xbox_series_xs: { xbox_series_xs_f: false, xbox_series_xs_t: true }
+  enum switch: { switch_f: false, switch_t: true }
+  enum smartphone: { smartphone_f: false, smartphone_t: true }
   
   has_many :favorites
   has_many :reverses_of_favorite, class_name: "Favorite", foreign_key: "user_id"
@@ -24,13 +32,9 @@ class Event < ApplicationRecord
     search_word_like(search_params[:search_word])
       .event_date_from(search_params[:event_date_from])
       .event_date_to(search_params[:event_date_to])
-      .place_like(search_params[:place])
-      # .content_like(search_params[:search_word])
   end
   
   scope :search_word_like, -> (search_word) { where("title LIKE ? OR content LIKE ?", "%#{search_word}%","%#{search_word}%") if search_word.present? }
   scope :event_date_from, -> (from) { where("? <= event_date", from) if from.present? }
   scope :event_date_to, -> (to) { where("event_date <= ?", to) if to.present? }
-  scope :place_like, -> (place) { where("place LIKE ?", "%#{place}%") if place.present? }
-  # scope :content_like, -> (search_word) { where("content LIKE ?", "%#{search_word}%") if search_word.present? }
 end
