71efd88523aa0cceaddc16933522b416d48dec1b
calpas7000 <calpas7000@gmail.com>
2020-11-23 15:18:46 +0000

    column add to view

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,8 @@ class ToppagesController < ApplicationController
   def index
     # @events = Event.all.order(event_date: "ASC")
     this_month_day = Time.zone.today
-    @this_month = Event.where(event_date: this_month_day.in_time_zone.all_month)
+    @this_month = Event.where(event_date: this_month_day.in_time_zone.all_month).order(event_date: "ASC").page(params[:page]).per(12)
     @next_month_day = Time.zone.today>>1
-    @next_month = Event.where(event_date: @next_month_day.in_time_zone.all_month)
+    @next_month = Event.where(event_date: @next_month_day.in_time_zone.all_month).order(event_date: "ASC").page(params[:page]).per(12)
   end
 end
